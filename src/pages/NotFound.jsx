cac7009d105652674ad23d546bbd153d09708a73
Kiku <likusa0110@gmail.com>
2026-03-26 20:57:32 +0800

    修改index.html語系標籤 lang 和NotFound頁面

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,13 @@
+import { Link } from 'react-router';
+
 function NotFound() {
 	return (
 		<>
-			<div className="container">
-				<h1 className="text-light">404 Not Found！</h1>
+			<div className="container py-5 d-flex flex-column align-items-center">
+				<h1 className="text-light text-center mb-4">404 Not Found！</h1>
+				<Link to="/" className="customer-link text-center">
+					回到首頁
+				</Link>
 			</div>
 		</>
 	);
