6205a8e68fef44832ad8b382fa80ae0a03c839f7
Kiku <likusa0110@gmail.com>
2026-04-02 20:24:05 +0800

    新增 ScrollRestoration 元件，跳轉頁面後可回到頁面最上方

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,10 +1,12 @@
-import { Outlet } from 'react-router';
+import { Outlet, ScrollRestoration } from 'react-router';
 import AdminNavbar from '../components/admin/AdminNavbar';
 import Footer from '../components/Footer';
 
 function AdminLayout() {
 	return (
 		<>
+			{/* 路徑改變時處理捲動歸零 */}
+			<ScrollRestoration />
 			<header className="bg-neutral-700">
 				<AdminNavbar />
 			</header>
--- src/layout/FrontedLayout.jsx
+++ src/layout/FrontedLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router';
+import { Outlet, ScrollRestoration } from 'react-router';
 import CarouselPromotion from '../components/CarouselPromotion';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -6,6 +6,8 @@ import Footer from '../components/Footer';
 function FrontedLayout() {
 	return (
 		<>
+			{/* 路徑改變時處理捲動歸零 */}
+			<ScrollRestoration />
 			<header>
 				<CarouselPromotion />
 				<Navbar />
